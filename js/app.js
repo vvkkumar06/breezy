41128f1be3f12699b0391a0d9f23ef888a24ad87
unknown <vivek.kumar@optimusinfo.com>
2019-03-03 12:49:09 +0530

    feat: comments added

--- js/app.js
+++ js/app.js
@@ -3,23 +3,31 @@ class App {
         this.views();
         this.routes();
     }
-
+    // Define routes
+    // component must be relative to views folder
+    // e.g. component: 'home' for home.html
+    // e.g. component: 'home/home' for home/home.html
     routes() {
         this.router = new Router([
             { component: 'home', active: true },
             { component: 'about' }
         ]);
     }
-
+    // Define views
+    // views must be relative to views folder
+    // e.g. header for views/header.html
     views() {
         views.generateViews([
-            'header'
+            'header',
+            'footer'
         ]);
     }
+    // Do not change this
+    // Initializing views
     view(view) {
         this.router.loadView(view);
         return views.load(view);
     }
 }
-var views = new Views();
-var app = new App();
+const views = new Views();
+const app = new App();
